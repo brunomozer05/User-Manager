2001e331a21b80fb917e93baa78316d56deaa720
Bruno Merico Mozer <brunomozer05@gmail.com>
2023-11-04 21:56:58 -0300

    Add Form Funcionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,12 @@ import styled from 'styled-components';
 import GlobalStyle from "./components/GlobalStyle";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import logo from './img/logo.png';
 import Form from "./components/Form.js";
+import Info from "./components/Info.js";
 
 function App() {
   return (
     <>
-      <img src={logo} height={100}/>
       <Form/>
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT}/>
       <GlobalStyle/>
--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,20 +1,19 @@
-import axios from "axios";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import { toast } from "react-toastify";
+import logo from "../img/logo.png";
 
-const Contents = styled.div`
+const FormBox = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-const FormBox = styled.div`
+const FormContents = styled.div`
   display: flex;
   flex-direction: row;
 `;
 
-const ContentLeft = styled.form`
+const ContentLeft = styled.div`
   color: white;
   font-weight: bold;
   display: grid;
@@ -23,7 +22,7 @@ const ContentLeft = styled.form`
   border-radius: 5px;
 `;
 
-const ContentRight = styled.form`
+const ContentRight = styled.div`
   color: white;
   font-weight: bold;
   display: grid;
@@ -35,6 +34,11 @@ const ContentRight = styled.form`
 const InputBox = styled.div`
   display: flex;
   flex-direction: column;
+  margin-top: 2rem;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
 `;
 
 const Input = styled.input`
@@ -60,36 +64,74 @@ const Button = styled.button`
   color: white;
   height: 3.2rem;
   border: none;
+  margin-top: 1.5rem;
+`;
+
+const Logo = styled.div`
+  width: 98vw;
 `;
 
 const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    birth_date: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    
+
+    const url = "http://localhost:8080/";
+  
+    const request = await fetch(url, {
+      body: JSON.stringify({
+        name: formData.name,
+        email: formData.email,
+        number: Number.parseInt(formData.phone),
+        birthDate: formData.birth_date,
+      }),
+      method: "POST",
+      headers: {"Content-Type": "application/json"}
+    });  
+  };
+
   return (
     <div>
-        <Contents>
-      <FormBox>
-        <ContentLeft>
-          <InputBox>
-            <Label>Name</Label>
-            <Input name="nome" />
-          </InputBox>
-          <InputBox>
-            <Label>E-mail</Label>
-            <Input name="email" type="email" />
-          </InputBox>
-        </ContentLeft>
-        <ContentRight>
-          <InputBox>
-            <Label>Phone</Label>
-            <Input name="phone" />
-          </InputBox>
-          <InputBox>
-            <Label>Birth Date</Label>
-            <Input name="birth_date" type="number" />
-          </InputBox>
-        </ContentRight>
+      <Logo>
+        <img src={logo} height={75} />
+      </Logo>
+      <FormBox onSubmit={onSubmit}>
+        <FormContents>
+          <ContentLeft>
+            <InputBox>
+              <Label>Name</Label>
+              <Input name="name" onChange={handleChange} value={formData.name} />
+            </InputBox>
+            <InputBox>
+              <Label>E-mail</Label>
+              <Input name="email" type="email" onChange={handleChange} value={formData.email} />
+            </InputBox>
+          </ContentLeft>
+          <ContentRight>
+            <InputBox>
+              <Label>Phone</Label>
+              <Input name="phone" onChange={handleChange} value={formData.phone} />
+            </InputBox>
+            <InputBox>
+              <Label>Birth Date</Label>
+              <Input name="birth_date" type="date" onChange={handleChange} value={formData.birth_date}/>
+            </InputBox>
+          </ContentRight>
+        </FormContents>
+        <Button type="submit">Submit</Button>
       </FormBox>
-      <Button type="submit">Submit</Button>
-      </Contents>
     </div>
   );
 };
