7c47745df7c582757b7a9bf027ad0d3b9e7c8665
Bruno Merico Mozer <brunomozer05@gmail.com>
2023-11-14 03:55:22 -0300

    Enhanced UI and Formatted JS

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -8,15 +8,37 @@ const DivTable = styled.div`
 `;
 
 const Table = styled.div`
-    display: flex;
+  display: flex;
+  flex-direction: column;
   margin-top: 4rem;
   background: #191a1a;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
   border-radius: 25px;
 `;
 
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
 const Titles = styled.div`
-color:white;
+margin-top: 1rem;
+  color: white;
+  font-size: 30px;
+  font-weight: 700;
+  margin-right: 5rem;
+  margin-left: 5rem;
+`;
+
+const Line = styled.div`
+  margin-left: 1rem;
+  margin-bottom: 1rem;
+  margin-top: 0.5rem;
+  width: 65rem;
+  height: 0.4rem;
+  border-radius: 15px;
+  opacity: 0.5;
+  background: rgba(0, 0, 0, 0.3);
 `;
 
 const Info = () => {
@@ -24,18 +46,14 @@ const Info = () => {
     <div>
       <DivTable>
         <Table>
-            <Titles>
-              Name  
-            </Titles>
-            <Titles>
-              E-mail 
-            </Titles>
-            <Titles>
-              Phone
-            </Titles>
-            <Titles>
-              Birth Date
-            </Titles>
+          <Header>
+            <Titles>Name</Titles>
+            <Titles>E-mail</Titles>
+            <Titles>Phone</Titles>
+            <Titles>Birth Date</Titles>
+          </Header>
+          <Line/>
+
         </Table>
       </DivTable>
     </div>
